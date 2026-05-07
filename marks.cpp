2ddd6d668729c63be9d5ce4e469b6e707f590b68
Rajat <rajatnatani0123@gmail.com>
2026-05-07 10:14:27 +0530

    Added marks.cpp

---
+++ marks.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int marks;
+    cout<< "enter your marks";
+    cin>> marks;
+    cout<< "your marks are: " << marks << endl;
+    return 0;
+}
